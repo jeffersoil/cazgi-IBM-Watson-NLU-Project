29682b3909db91d928496c893876aa2c80785bf0
jefferson <jefissu@gmail.com>
2022-02-17 13:05:47 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
   value of the state, will be returned. The initial input mode
   is set to text
   */
+    
+
   state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
             mode: "text",
           sentimentOutput:[],
@@ -21,7 +23,7 @@ class App extends React.Component {
   If the requested input mode is "text" it returns a textbox with 4 rows.
   If the requested input mode is "url" it returns a textbox with 1 row.
   */
- 
+  
   renderOutput = (input_mode)=>{
     let rows = 1
     let mode = "url"
@@ -35,6 +37,8 @@ class App extends React.Component {
       sentimentOutput:[],
       sentiment:true
       });
+
+      document.title = "Sentiment Analyzer";
   } 
   
   sendForSentimentAnalysis = () => {
@@ -49,9 +53,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green";break;
+          case "negative": color = "red";break;
+          default: color = "yellow";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
